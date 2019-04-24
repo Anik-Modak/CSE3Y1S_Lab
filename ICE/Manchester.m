985377c07e69ed039f97faf71834b1c90d490704
Anik Modak <37172268+Anik-Modak@users.noreply.github.com>
2019-04-23 21:32:47 -0700

    RZ updated and Manchester and Differential Manchester
    RZ updated and Manchester and Differential Manchester

--- ICE/Manchester.m
+++ ICE/Manchester.m
@@ -1,52 +1,73 @@
 clear;
-data = [0,0,0,0,1,0];
 
-k=1;
-t=0;
-bitrate=0.5;
-sign = 1;
-Time=length(data);
-samplesize = 100000;
+bits = [0,1,0,0,1,1];
 
 prompt = 'What is the voltage? ';
 voltage = input(prompt);
+bit_rate=0.5;
+sign = -1;
 
-for j=1:length(data)
-    for i=0:(1/samplesize):bitrate/2.0
-        time(k) = i+t;
-        if data(j)== 1
-            result(k) = -sign * voltage;
-        else
-            result(k) = sign * voltage;
-        end
-        k = k+1;
-    end
-    for p=i:1/samplesize:bitrate
-        time(k)=t+p;
-        result(k)=0;
-        k=k+1;
-    end
-    t= time(k-1);
-end;
 
-title('RZ');
+tmp = voltage;
+in = 1;
+voltage = sign*voltage;
+
+for i = 1:length(bits)
+    if(bits(i)==0)
+        y_level(in) = voltage;
+        y_level(in+1) = -voltage;
+    else
+        y_level(in) = -voltage;
+        y_level(in+1) = voltage;
+    end
+    in = in + 2;
+end
+
+frequency = 1000;
+voltage=tmp;
+bit_rate = bit_rate*2;
+Time=length(bits)*2/bit_rate;
+dt = 1/frequency;
+time = 0:dt:Time;
+
+x = 1;
+for i = 1:length(time)
+    result(i)= y_level(x);
+    if time(i)*bit_rate>=x
+        x= x+1;
+    end
+end
+
+
 plot(time,result);
-axis([0 Time -2*voltage 2*voltage]);
+axis([0 Time -voltage*2 voltage*2]);
+
 
 % demodulation
-x = 1;
-in = 1;
-for i = 1:length(time)
-    if time(i) > x*bitrate
-        x = x + 1;
-        if result(i)/voltage>0
-            ans_bits(x)=0;
-        else
-            ans_bits(x)=1;
-        end
-    end
-end;
+i=1;
+in=1;
+st=1;
+tmp=1*sign;
 
-disp(data);
-disp('Demodulation : ')
+for j=1:length(time)
+    dm = result(j)/voltage;
+    if time(j)*bit_rate>=i
+        
+        if mod(in,2)==1
+            if dm ==tmp
+                ans_bits(st)=0;
+            else
+                ans_bits(st)=1;
+            end
+            st = st + 1;
+        end
+        
+        i = i + 1;
+        in = in + 1;
+    end
+end
+
+title('Manchester');
+disp(bits);
+disp('Demodulation : ');
 disp(ans_bits);
--- ICE/differential_manchester.m
+++ ICE/differential_manchester.m
@@ -1,5 +1,6 @@
 clear;
-bits = [0,1,0,0,1];
+
+bits = [0,1,0,0,1,1];
 
 prompt = 'What is the voltage? ';
 voltage = input(prompt);
@@ -7,33 +8,32 @@ voltage = input(prompt);
 prompt = 'What is the bit_rate? ';
 bit_rate = input(prompt);
 
-frequency = 1000;
 sign = 1;
-
-
 tmp=voltage;
 in = 1;
-voltage = voltage*sign;
+voltage = sign*voltage;
 
 for i = 1:length(bits)
     if(bits(i)==0)
-        y_level(in) = voltage;
-        y_level(in+1) = 0;
-    else
         y_level(in) = -voltage;
-        y_level(in+1) = 0;
+        y_level(in+1) = voltage;
+    else
+        y_level(in) = voltage;
+        y_level(in+1) = -voltage;
+        voltage = -voltage;
     end
     in = in + 2;
 end
 
+
+frequency = 1000;
 voltage=tmp;
 bit_rate = bit_rate*2;
-Time=length(bits)*2/bit_rate;
+Time = length(bits)*2/bit_rate;
 dt = 1/frequency;
 time = 0:dt:Time;
 
 x = 1;
-
 for i = 1:length(time)
     y_value(i)= y_level(x);
     if time(i)*bit_rate>=x
@@ -41,36 +41,34 @@ for i = 1:length(time)
     end
 end
 
-
 plot(time,y_value);
 axis([0 Time -voltage*2 voltage*2]);
 
-
 % demodulation
-
 i=1;
 in=1;
 st=1;
 tmp=1*sign;
+
 for j=1:length(time)
     dm = y_value(j)/voltage;
     if time(j)*bit_rate>=i
         if mod(in,2)==1
-            if dm ==tmp
+            if dm ~=tmp
                 ans_bits(st)=0;
             else
                 ans_bits(st)=1;
+                tmp =  -tmp; %middle a trasition ghotlo
             end
             st = st + 1;
         end
-        
         i=i+1;
         in= in+1;
     end
 end
 
-title('RZ');
+title('Differential-Manchester');
 disp('Orginal bits : ')
 disp(bits);
-disp('Demodulation : ');
-disp(ans_bits);
+disp('Demodulation : ')
+disp(ans_bits)
